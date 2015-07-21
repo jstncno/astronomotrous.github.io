b576d9d24d9704aceb1d5ba0ad8eb7ef79224b58
Justin Cano <jcano001@ucr.edu>
2015-07-21 12:02:33 -0700

    updated about me description

--- js/app.js
+++ js/app.js
@@ -15,8 +15,8 @@
 
   var headerBlocks = ["Hello, world! My name is Justin Cano.",
                       "I'm a recent graduate of the University of California, Riverside. I've obtained my BS Degree in Computer Engineering in the Spring of 2014.",
-                      "I like to solve puzzles, do coding challenges, and learn new technologies. I am seeking an intensive software engineering position in the SF Bay Area, CA that will challenge me to put my skills to the test and provide me with the guidance to develop best software engineering practices."
-                          ];
+                      "I like to solve puzzles, do coding challenges, and learn new technologies. I am currently seeking an intensive software or data engineering position in the SF Bay Area, CA that will challenge me to put my skills to the test and provide me with the guidance to develop best software engineering practices."
+                      ];
 
   var projects = [{
                 "name" : "Data Engineering Fellow @ Insight Data Science",
