dc4efdf8aaccd57887c4f76e5136b5d8ea326fb1
Justin Cano <jcano001@ucr.edu>
2015-09-28 15:34:43 -0700

    added material-ui react components

---
+++ src/src/app/components/grid.jsx
@@ -0,0 +1,70 @@
+/** In this file, we create a React component which incorporates components provided by material-ui */
+
+let React = require('react');
+let mui = require('material-ui');
+let RaisedButton = mui.RaisedButton;
+let Dialog = mui.Dialog;
+let AppBar = mui.AppBar;
+let Card = mui.Card;
+let CardText = mui.CardText;
+let FloatingActionButton = mui.FloatingActionButton;
+let FontIcon = mui.FontIcon;
+let ThemeManager = new mui.Styles.ThemeManager();
+let Colors = mui.Styles.Colors;
+
+let Grid = React.createClass({
+
+  childContextTypes: {
+    muiTheme: React.PropTypes.object
+  },
+
+  getChildContext() {
+    return {
+      muiTheme: ThemeManager.getCurrentTheme()
+    };
+  },
+
+  componentWillMount() {
+    ThemeManager.setPalette({
+      accent1Color: Colors.deepOrange500
+    });
+  },
+
+  render() {
+
+    return (
+      <div>
+        <Card className="col-md-4 grid-item">
+          <CardText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+          </CardText>
+        </Card>
+
+        <Card className="col-md-4 grid-item">
+          <CardText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+          </CardText>
+        </Card>
+
+        <Card className="col-md-4 grid-item">
+          <CardText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+          </CardText>
+        </Card>
+
+      </div>
+    );
+  },
+
+});
+
+module.exports = Grid;
---
+++ src/src/app/components/main.jsx
@@ -0,0 +1,109 @@
+/** In this file, we create a React component which incorporates components provided by material-ui */
+
+let React = require('react');
+let mui = require('material-ui');
+let RaisedButton = mui.RaisedButton;
+let Dialog = mui.Dialog;
+let AppBar = mui.AppBar;
+let Card = mui.Card;
+let CardText = mui.CardText;
+let LeftNav = mui.LeftNav;
+let MenuItem = mui.MenuItem;
+let ThemeManager = new mui.Styles.ThemeManager();
+let Colors = mui.Styles.Colors;
+
+let Main = React.createClass({
+
+  childContextTypes: {
+    muiTheme: React.PropTypes.object
+  },
+
+  getChildContext() {
+    return {
+      muiTheme: ThemeManager.getCurrentTheme()
+    };
+  },
+
+  componentWillMount() {
+    ThemeManager.setPalette({
+      accent1Color: Colors.deepOrange500
+    });
+  },
+
+  toggleMenu() {
+    this.refs.leftNav.toggle();
+  },
+
+  render() {
+
+    let menuItems = [
+      { route: 'get-started', text: 'Get Started' },
+      { route: 'customization', text: 'Customization' },
+      { route: 'components', text: 'Components' },
+      { type: MenuItem.Types.SUBHEADER, text: 'Resources' },
+      {
+         type: MenuItem.Types.LINK,
+         payload: 'https://github.com/callemall/material-ui',
+         text: 'GitHub'
+      },
+      {
+         text: 'Disabled',
+         disabled: true
+      },
+      {
+         type: MenuItem.Types.LINK,
+         payload: 'https://www.google.com',
+         text: 'Disabled Link',
+         disabled: true
+      },
+    ];
+
+    return (
+      <div>
+        <LeftNav ref="leftNav" docked={false} menuItems={menuItems} />
+        <div>
+          <AppBar
+            title="Title"
+            onLeftIconButtonTouchTap={this.toggleMenu} />
+        </div>
+
+        <div>
+          <Card className="col-md-4 grid-item">
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+              Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+              Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+            </CardText>
+          </Card>
+
+          <Card className="col-md-4 grid-item">
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+              Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+              Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+            </CardText>
+          </Card>
+
+          <Card className="col-md-4 grid-item">
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+              Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+              Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+            </CardText>
+          </Card>
+        </div>
+
+      </div>
+    );
+  },
+
+  _handleTouchTap() {
+    this.refs.superSecretPasswordDialog.show();
+  }
+
+});
+
+module.exports = Main;
