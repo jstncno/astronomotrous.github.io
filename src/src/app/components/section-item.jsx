a00f0bdf6e5474c406127c9352d39035858b2c18
Justin Cano <jcano001@ucr.edu>
2015-10-14 22:18:06 -0700

    added own column width style (instead of relying on bootstrap

--- src/src/app/components/section-item.jsx
+++ src/src/app/components/section-item.jsx
@@ -48,6 +48,27 @@ let SectionItem = React.createClass({
       "text-align": "right"
     }
 
+    let col4 = {
+      "max-width": "33.33333333%",
+      "min-height": "1px",
+      "padding-right": "15px",
+      "padding-left": "15px"
+    }
+
+    let col8 = {
+      "max-width": "66.66666667%",
+      "min-height": "1px",
+      "padding-right": "15px",
+      "padding-left": "15px"
+    }
+
+    let col12 = {
+      "max-width": "100%",
+      "min-height": "1px",
+      "padding-right": "15px",
+      "padding-left": "15px"
+    }
+
     return (
       <div
        className="section-item"
@@ -56,17 +77,17 @@ let SectionItem = React.createClass({
 
         <Paper className="paper-item" zDepth={this.state.zDepth}>
           <div className="row">
-            <div className="col-md-12">
+            <div style={col12}>
               <img className="section-img" src={this.props.data.imgUrl} />
             </div>
           </div>
 
           <div className="row">
-            <div className="col-md-8">
+            <div style={col8}>
               <h3 className="section-title"><a href={this.props.data.companyUrl}>{this.props.data.company}</a></h3>
               <h4 className="section-subtitle">{this.props.data.title}</h4>
             </div>
-            <div className="col-md-4" style={dateStyle}>
+            <div style={col4} style={dateStyle}>
               <h4>{this.props.data.date}</h4>
             </div>
           </div>
